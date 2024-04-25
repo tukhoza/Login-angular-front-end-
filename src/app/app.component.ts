8f39fcdab521ade2c929ce4cdf3074156fa6bc79
MID-3WDPXY2\BigBoss <tumelokhoza3@gmail.com>
2024-04-25 20:21:10 +0200

    Add Project Login to the github

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent implements OnInit{
+  title = 'login';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
+}
