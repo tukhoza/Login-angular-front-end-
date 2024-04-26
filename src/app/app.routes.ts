1c0ff00014caeb01dc6b009a64d242d65ab9fe7a
MID-3WDPXY2\BigBoss <tumelokhoza3@gmail.com>
2024-04-26 08:24:24 +0200

    Sticky navbar and added the login route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AuthenticationPageComponent } from './pages/authentication-page/authentication-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "auth", component: AuthenticationPageComponent}
+];
