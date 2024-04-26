fe7e2cc373072ee88cdad26e22dfcdbd6d9d0eb8
MID-3WDPXY2\BigBoss <tumelokhoza3@gmail.com>
2024-04-26 08:16:19 +0200

    Added new Folders for layout, components and pages. separated the component on the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
+import { AuthenticationPageComponent } from './pages/authentication-page/authentication-page.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    AuthenticationPageComponent,
+    FooterComponent,
+    NavBarComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
